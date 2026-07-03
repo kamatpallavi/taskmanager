0ee9d60c50ad0fcff193dea3bebae96a22fe9edb
kamatpallavi <kamatpallavi65@gmail.com>
2026-07-03 10:37:01 +0530

    1.code for add task(html)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Register } from './register/register';
 import { Login } from './login/login';
+import { AddTask } from './add-task/add-task';
 
 export const routes: Routes = [
   {
@@ -9,5 +10,6 @@ export const routes: Routes = [
   },
 
     { path: 'login', component: Login },
-    { path: 'register', component: Register }
+    { path: 'register', component: Register },
+    { path: 'add-task', component: AddTask }
 ];
